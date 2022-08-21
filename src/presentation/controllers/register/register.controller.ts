3da29d33adaab28e05fc1c013dafa5d23b9be27b
Daniel <soumenorgame@gmail.com>
2022-08-21 19:25:55 -0300

    feat: crypto passworx

--- src/presentation/controllers/register/register.controller.ts
+++ src/presentation/controllers/register/register.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UseInterceptors,
+} from '@nestjs/common';
 import { RegisterDto } from 'src/presentation/dtos';
 import { RegisterService } from 'src/usecases/register/register.service';
 
@@ -7,6 +13,7 @@ export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
 
   @Post()
+  @UseInterceptors(UnauthorizedException)
   async register(@Body() params: RegisterDto) {
     const user = await this.registerService.register(params);
     return { id: user.identifiers[0].id };
--- src/usecases/register/register.service.ts
+++ src/usecases/register/register.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { createHash } from 'crypto';
 import { User } from 'src/infra/typeorm/entities';
 import { RegisterDto } from 'src/presentation/dtos';
 import { Repository } from 'typeorm';
@@ -13,6 +14,11 @@ export class RegisterService {
   async register({ email, name, password }: RegisterDto) {
     const user = await this.userRepository.findOneBy({ email });
     if (user) throw new UnauthorizedException('email already registered');
-    return await this.userRepository.insert({ email, name, password });
+    const hashedPassword = createHash('sha256').update(password).digest('hex');
+    return await this.userRepository.insert({
+      email,
+      name,
+      password: hashedPassword,
+    });
   }
 }
