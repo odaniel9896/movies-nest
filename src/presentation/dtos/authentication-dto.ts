6477326f6a31ed3d007ad914a78e622e4f66d4c4
Daniel <soumenorgame@gmail.com>
2022-08-21 19:38:31 -0300

    feat: add swagger

--- src/presentation/dtos/authentication-dto.ts
+++ src/presentation/dtos/authentication-dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class AuthenticationDto {
   @IsString()
   @IsEmail()
+  @ApiProperty({ required: true, default: 'email@email.com' })
   email: string;
 
   @IsString()
+  @ApiProperty({ required: true, default: '123456789' })
   password: string;
 }
--- src/presentation/dtos/register-dto.ts
+++ src/presentation/dtos/register-dto.ts
@@ -1,13 +1,17 @@
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
   @IsEmail()
+  @ApiProperty({ required: true, default: 'email@primi.com.br' })
   email: string;
 
   @IsString()
+  @ApiProperty({ required: true, default: '123456789' })
   password: string;
 
   @IsString()
+  @ApiProperty({ required: true, default: 'Daniel Vitor' })
   name: string;
 }
