db3af1939327f996df2b43f0a16a9dfae36b1ddf
Daniel <soumenorgame@gmail.com>
2022-08-24 13:22:25 -0300

    feat: add sqlite

--- src/infra/typeorm/config/database.providers.ts
+++ src/infra/typeorm/config/database.providers.ts
@@ -6,12 +6,12 @@ export const databaseProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
-        type: 'mysql',
-        host: constants().database.DB_HOST,
-        port: parseInt(constants().database.DB_PORT, 10),
-        username: constants().database.DB_USERNAME,
-        password: constants().database.DB_PASSWORD,
-        database: constants().database.DB_DATABASE,
+        type: 'sqlite',
+        // host: constants().database.DB_HOST,
+        // port: parseInt(constants().database.DB_PORT, 10),
+        //username: constants().database.DB_USERNAME,
+        //password: constants().database.DB_PASSWORD,
+        database: 'movies.sqlite',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
