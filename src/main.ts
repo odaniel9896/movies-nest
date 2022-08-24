83290a372249101d73c7a4bf7a407ac4875b07c7
Daniel <soumenorgame@gmail.com>
2022-08-24 14:06:22 -0300

    chore: changes to heroku

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import constants from './main/config/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(constants().port || 3000);
 }
 bootstrap();
--- src/main/config/constants.ts
+++ src/main/config/constants.ts
@@ -1,11 +1,11 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET ?? '',
   database: {
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    DB_DATABASE: process.env.DB_DATABASE,
+    DB_PASSWORD: process.env.DB_PASSWORD ?? '',
+    DB_USERNAME: process.env.DB_USERNAME ?? '',
+    DB_HOST: process.env.DB_HOST ?? '',
+    DB_PORT: process.env.DB_PORT ?? '',
+    DB_DATABASE: process.env.DB_DATABASE ?? '',
   },
 });
